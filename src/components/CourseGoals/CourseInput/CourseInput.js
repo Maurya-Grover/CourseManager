903bc9c643f3ee6335dbea3f70db7c722e40ae2c
Maurya-Grover <maurya.grover@gmail.com>
2022-02-15 00:54:26 +0530

    Add static inline styles

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,33 @@ import "./CourseInput.css";
 
 const CourseInput = (props) => {
 	const [enteredValue, setEnteredValue] = useState("");
+	const [isValid, setIsValid] = useState(true);
 
 	const goalInputChangeHandler = (event) => {
+		if (event.target.value.trim().length > 0) setIsValid(true);
 		setEnteredValue(event.target.value);
 	};
 
 	const formSubmitHandler = (event) => {
 		event.preventDefault();
-		props.onAddGoal(enteredValue);
+		if (enteredValue.trim().length > 0) props.onAddGoal(enteredValue);
+		else setIsValid(false);
+		setEnteredValue("");
 	};
 
 	return (
 		<form onSubmit={formSubmitHandler}>
 			<div className="form-control">
-				<label>Course Goal</label>
-				<input type="text" onChange={goalInputChangeHandler} />
+				<label style={{ color: isValid ? "black" : "red" }}>Course Goal</label>
+				<input
+					value={enteredValue}
+					type="text"
+					onChange={goalInputChangeHandler}
+					style={{
+						borderColor: isValid ? "#ccc" : "red",
+						background: isValid ? "transparent" : "salmon",
+					}}
+				/>
 			</div>
 			<Button type="submit">Add Goal</Button>
 		</form>
